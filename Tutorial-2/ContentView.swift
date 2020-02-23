ea429792540425a7435271778f222231042c1f5b
Sreenikhil <kollu1996@gmail.com>
2020-02-23 16:44:47 -0500

    Initial Commit

---
+++ Tutorial-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial-2
+//
+//  Created by Sreenikhil Kollu on 2/23/20.
+//  Copyright © 2020 Sreenikhil Kollu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
